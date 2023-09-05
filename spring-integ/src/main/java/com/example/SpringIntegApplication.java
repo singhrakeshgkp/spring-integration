3bffe830de8448a06356b6f8fa0e364f1ad59b43
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2023-09-05 12:10:26 -0700

    created custom integrtion flow and custom message source

--- spring-integ/src/main/java/com/example/SpringIntegApplication.java
+++ spring-integ/src/main/java/com/example/SpringIntegApplication.java
@@ -1,6 +1,11 @@
 package com.example;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,43 +14,63 @@ import org.springframework.integration.core.GenericTransformer;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.dsl.context.IntegrationFlowContext;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringIntegApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringIntegApplication.class, args);
-	}
-
-	/*Message channel is kind of pipe, the message moves through channel to components
-	* to another channel to another component so on until it eventually terminates.*/
-	@Bean
-	MessageChannel myChannel(){
-		return MessageChannels.direct().getObject();
-	}
-
-  @Bean
-  IntegrationFlow flow() {
-    return IntegrationFlow.from(
-            (MessageSource<String>)
-                () ->
-                    MessageBuilder.withPayload(
-                            "Hi welcome to spring integration demo " + Instant.now() + " ")
-                        .build(),
-            poller -> poller.poller(pm -> pm.fixedRate(1000)))
-				/*Additional steps so we can keep on going. this will transfor the msg in upper case*/
-				.transform((GenericTransformer<String, String>) source -> source.toUpperCase())
-				.handle(
-						(GenericHandler<String>)
-								(payload, headers) -> {
-									System.out.println("payload is :- "+payload);
-									return null;//returns null its also terminate/end of the flow
-								})
-        .get();
+  public static void main(String[] args) {
+    SpringApplication.run(SpringIntegApplication.class, args);
   }
 
+  /*Message channel is kind of pipe, the message moves through channel to components
+   * to another channel to another component so on until it eventually terminates.*/
+  @Bean
+  MessageChannel myChannel() {
+    return MessageChannels.direct().getObject();
+  }
+
+	private static String msg(){
+		double number =  Math.random();
+		return number >.29 ?"Hi This is Even Number "+Instant.now()+" number "+number+"": "Hi This is Odd number, and number is "+number;
+	}
+
+	@Component
+	static class MsgSource  implements  MessageSource<String>{
+
+		@Override
+		public Message<String> receive() {
+			return MessageBuilder.withPayload(msg()).build();
+		}
+	}
+
+	@Bean
+	ApplicationRunner runner(MsgSource msgSource, IntegrationFlowContext context){
+		return args -> {
+var evenFlow = getFlow(msgSource,1,"Even");
+		 var oddFlow = getFlow(msgSource,3,"Odd");
+			List.of(evenFlow,oddFlow).forEach(
+					flow -> context.registration(flow).register().start());
+		};
+	}
+
+
+  IntegrationFlow getFlow(MessageSource messageSource, int seconds, String filteredMsg) {
+    return IntegrationFlow.from(messageSource,
+            poller -> poller.poller(pm -> pm.fixedRate(seconds*1000)))
+				.filter(String.class,source -> source.contains(filteredMsg))
+        .transform((GenericTransformer<String, String>) source -> source.toUpperCase())
+        .handle(
+            (GenericHandler<String>)
+                (payload, headers) -> {
+                  System.out.println("payload for filtered msg [ " +filteredMsg+"] is, "+ payload);
+                  return null; // returns null its also terminate/end of the flow
+                })
+        .get();
+  }
 }
